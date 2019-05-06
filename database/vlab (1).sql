7bf7739c80d14cf6a1d8f37b30aa25ebfc595089
luuvanmy <my08091993@gmail.com>
2019-05-06 21:46:59 +0700

    vlab 3

--- database/vlab (1).sql
+++ database/vlab (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2019 at 04:27 PM
+-- Generation Time: May 06, 2019 at 04:46 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.4
 
@@ -48,19 +48,6 @@ CREATE TABLE `briefs` (
 
 INSERT INTO `briefs` (`id`, `code`, `name`, `page_num`, `num`, `status`, `note`, `district`, `ward`, `created_at`, `updated_at`) VALUES
 ('18571557070494', '18571557070494', 'dfdsf', 12, 12, 'watting_approval', 'mình có nhận xét', 'er', 'wer', 1557070494, 1557070494),
-('224511557069383', '', 'dsfds', 324, 34534, 'watting_approval', 'dfsdfsd', 'df', 'dsfsd', 1557069383, 1557069383),
-('263841557069910', '', 'hj', 1, 1, 'in_process', '', 'jhj', 'jhj', 1557069910, 1557069910),
-('27611557068711', '', 'ten', 2, 423423, 'in_process', '', 'dfsd', 'ses', 1557068711, 1557068711),
-('301641557069677', '', 'dfdgd', 1212, 12321, 'in_process', '', 'ewrew', 'were', 1557069677, 1557069677),
-('30641557070324', '', 'dfdsf', 123, 123, 'in_process', '', 'fewrw', 'ewrwe', 1557070324, 1557070324),
-('329561557052251', '', 'Luu van my', 234, 456, 'wait', '', 'dt', 'er', 1557052251, 1557052251),
-('368961557068880', '', 'fdf', 12, 132, 'in_process', '', 'werew', 'efewf', 1557068880, 1557068880),
-('382971557068793', '', 'ten', 2, 423423, 'in_process', '', 'dfsd', 'ses', 1557068793, 1557068793),
-('515471557069252', '', 'fdsfd', 435344345, 435345, 'in_process', '', 'dsfd', 'dsfsdf', 1557069252, 1557069252),
-('753721557070303', '', 'dfdsf1', 12313, 123, 'in_process', '', 'sdf', 'sds', 1557070303, 1557070303),
-('804271557069824', '', 'dfdf', 1, 2, 'in_process', '', 'dgdf', 'gtreter', 1557069824, 1557069824),
-('870541557069311', '', 'gdfg', 4534, 345, 'in_process', '', 'dfgd', 'fdgdf', 1557069311, 1557069311),
-('899091557070620', '899091557070620', 'dfdsf', 1, 20, 'in_process', '', 'dfs', 'sdfsd', 1557070620, 1557070620),
 ('993131557052235', '', 'Luu van my', 234, 456, 'wait', '', 'dt', 'er', 1557052235, 1557052235),
 ('abc123', '', 'luu van my', 123, 1234, 'wait', '', 'quận 7', 'phường 3', 1557032352, 1557032352);
 
